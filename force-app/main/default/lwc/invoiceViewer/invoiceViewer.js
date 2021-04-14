89f1f2d4b447d7962875af52cca39e8f1716c5cf
JD <jdority@salesforce.com>
2021-04-14 12:35:22 -0700

    more changes

--- force-app/main/default/lwc/invoiceViewer/invoiceViewer.js
+++ force-app/main/default/lwc/invoiceViewer/invoiceViewer.js
@@ -1,7 +1,6 @@
 import { LightningElement, api, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 import communityBasePath from '@salesforce/community/basePath';
-import communityId from '@salesforce/community/Id';
 import APEX_getAllInvoices from '@salesforce/apex/InvoiceViewerController.getAllInvoices'; // no params
 
 const cols = [
@@ -47,32 +46,57 @@ const cols = [
 export default class InvoiceViewer extends NavigationMixin(LightningElement) {
     @track columns = cols;
     @api invoices;
+    @track selectedInvoices = [];
 
     connectedCallback() {
-        // this.isLoading();
-
         APEX_getAllInvoices().then(results => {
             let tempInvoices = [...results];
             const domain = window.location.origin;
             const base = communityBasePath;
             const url = domain + base;
-            console.log(url);
 
             tempInvoices.map(x => x.link = url + '/detail/' + x.Id)
             this.invoices = tempInvoices;
-            console.log(this.invoices);
-
-            // this.sfdcBaseURL = window.location.origin;
-            //   this.isLoaded();
         });
+    }
+
+    handleRowUpdate(event) {
+        const selectedRows = event.target.selectedRows;
+        let rows = [];
+        
+        for (var i = 0; i < selectedRows.length; i++){
+            let rowIndex = Number(selectedRows[i].replace(/\D/g, ''));
+            rows.push(rowIndex);
+        }
+
+        let selected = this.invoices.filter((item, index) => rows.includes(index));
+        this.selectedInvoices = selected;
     }
 
     payInvoices() {
+        const baseURL = 'https://trane.com/';
+        const params =  this.selectedInvoices.length > 0
+                        ? this.setInvoiceNumbers(this.selectedInvoices)
+                        : '';
+        const url = baseURL + params;
+
         this[NavigationMixin.Navigate]({
             "type": "standard__webPage",
             "attributes": {
-                "url": "https://trane.com"
+                "url": url
             }
         });
     }
+
+    setInvoiceNumbers(invoices) {
+        let retString = '?invoices=';
+        const length = invoices.length;
+        invoices.forEach(element => {
+            element === invoices[length -1] 
+            ? retString += element.Name
+            : retString += (element.Name + ',');
+        });
+        return retString;
+    }
+
 }
